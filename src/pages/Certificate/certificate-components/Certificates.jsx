f0e95a123dfbe13ad62d0484763ff68d1eb975d8
Amruta Sawant <amrutasawant492@gmail.com>
2024-10-14 18:46:36 +0530

    new form

--- src/pages/Certificate/certificate-components/Certificates.jsx
+++ src/pages/Certificate/certificate-components/Certificates.jsx
@@ -26,6 +26,16 @@ const Certificates = () => {
           className="w-full h-[400px] sm:h-[1200px]"
         />
       </div>
+      <div>
+        <h1 className="text-center sm:text-3xl text-xl p-7">
+          Importer Exporter Code
+        </h1>
+        <img
+          src="./assets/u4.jpg"
+          alt=""
+          className="w-full h-[400px] sm:h-[1200px]"
+        />
+      </div>
     </div>
   );
 };
--- src/pages/home/home-components/Pillars.jsx
+++ src/pages/home/home-components/Pillars.jsx
@@ -11,27 +11,28 @@ const Pillars = () => {
 
             <div class="flex flex-wrap justify-center gap-14 mt-10">
                 {/* <!-- Card 1 --> */}
-                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-64 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl">
-                    <span class="absolute top-0 left-0 z-0 h-32 w-32 rounded-full bg-gradient-to-r from-purple-500 to-amber-500 opacity-75 transition-all duration-500 transform group-hover:scale-[20]"></span>
-                    <div class="relative z-10 mx-auto max-w-md">
-                        <span class="grid h-32 w-32 place-items-center rounded-full bg-gradient-to-r from-purple-500 to-blue-500 transition-all duration-500 transform group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:to-yellow-500">
+                <div className="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-80 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl hover:bg-orange-500"> {/* Add hover:bg-orange-500 */}
+                    <span className="absolute top-0 left-0 z-0 h-32 w-32 rounded-full bg-gradient-to-r from-purple-500 to-amber-500 opacity-75 transition-all duration-500 transform group-hover:scale-[20]"></span>
+                    <div className="relative z-10 mx-auto max-w-md">
+                        <span className="grid h-32 w-32 place-items-center rounded-full bg-gradient-to-r from-purple-500 to-blue-500 transition-all duration-500 transform group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:to-yellow-500">
                             <FaSackDollar className="h-16 w-16 text-white transition-all" />
                         </span>
-                        <div class="space-y-6 pt-6 text-lg leading-8 text-gray-700 transition-all duration-500 group-hover:text-white">
-                            <p class="font-medium">
+                        <div className="space-y-6 pt-6 text-lg leading-8 text-gray-700 transition-all duration-500 group-hover:text-white">
+                            <p className="font-medium">
                                 Flexible financial products tailored for small and medium-sized farms. Financial literacy programs to empower farmers in managing their finances effectively.
                             </p>
                         </div>
-                        <div class="pt-6 text-lg font-semibold leading-7">
+                        <div className="pt-6 text-lg font-semibold leading-7">
                             <p>
-                                <span class="text-amber-500 transition-all duration-500 group-hover:text-white">Accessible Financing Solutions</span>
+                                <span className="text-amber-500 transition-all duration-500 group-hover:text-white">Accessible Financing Solutions</span>
                             </p>
                         </div>
                     </div>
                 </div>
 
+
                 {/* <!-- Card 2 --> */}
-                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-64 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl">
+                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-80 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl hover:bg-orange-500">
                     <span class="absolute top-0 left-0 z-0 h-32 w-32 rounded-full bg-gradient-to-r from-purple-500 to-amber-500 opacity-75 transition-all duration-500 transform group-hover:scale-[20]"></span>
                     <div class="relative z-10 mx-auto max-w-md">
                         <span class="grid h-32 w-32 place-items-center rounded-full bg-gradient-to-r from-purple-500 to-blue-500 transition-all duration-500 transform group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:to-yellow-500">
@@ -52,7 +53,7 @@ const Pillars = () => {
                 </div>
 
                 {/* <!-- Card 3 --> */}
-                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-64 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl">
+                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-80 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl hover:bg-orange-500">
                     <span class="absolute top-0 left-0 z-0 h-32 w-32 rounded-full bg-gradient-to-r from-purple-500 to-amber-500 opacity-75 transition-all duration-500 transform group-hover:scale-[20]"></span>
                     <div class="relative z-10 mx-auto max-w-md">
                         <span class="grid h-32 w-32 place-items-center rounded-full bg-gradient-to-r from-purple-500 to-blue-500 transition-all duration-500 transform group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:to-yellow-500">
@@ -60,8 +61,8 @@ const Pillars = () => {
                         </span>
                         <div class="space-y-6 pt-6 text-lg leading-8 text-gray-700 transition-all duration-500 group-hover:text-white">
                             <p class="font-medium">
-                            Adoption of advanced agricultural technologies (e.g., precision farming, smart irrigation).
-                            Investment in research and development to improve crop yields and sustainability.
+                                Adoption of advanced agricultural technologies (e.g., precision farming, smart irrigation).
+                                Investment in research and development to improve crop yields and sustainability.
                             </p>
                         </div>
                         <div class="pt-6 text-lg font-semibold leading-7">
@@ -73,7 +74,7 @@ const Pillars = () => {
                 </div>
 
                 {/* <!-- Card 4 --> */}
-                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-64 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl">
+                <div class="group relative cursor-pointer overflow-hidden bg-amber-50 rounded-2xl w-80 px-6 pt-12 pb-10 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-500 transform hover:scale-105 hover:shadow-3xl hover:bg-orange-500">
                     <span class="absolute top-0 left-0 z-0 h-32 w-32 rounded-full bg-gradient-to-r from-purple-500 to-amber-500 opacity-75 transition-all duration-500 transform group-hover:scale-[20]"></span>
                     <div class="relative z-10 mx-auto max-w-md">
                         <span class="grid h-32 w-32 place-items-center rounded-full bg-gradient-to-r from-purple-500 to-blue-500 transition-all duration-500 transform group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:to-yellow-500">
@@ -81,8 +82,8 @@ const Pillars = () => {
                         </span>
                         <div class="space-y-6 pt-6 text-lg leading-8 text-gray-700 transition-all duration-500 group-hover:text-white">
                             <p class="font-medium">
-                            Training programs focused on modern farming techniques and best practices.
-                            Workshops and seminars on financial management, marketing, and business development.
+                                Training programs focused on modern farming techniques and best practices.
+                                Workshops and seminars on financial management, marketing, and business development.
                             </p>
                         </div>
                         <div class="pt-6 text-lg font-semibold leading-7">
